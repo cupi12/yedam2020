f02c8379c8da263e1d942434fae93471cbc75024
User <User@YD02-07>
2020-02-05 17:49:54 +0900

    0205

--- HelloWorld/src/com/yedam/hjw/api/string연습/StringBuilderExample.java
+++ HelloWorld/src/com/yedam/hjw/api/string연습/StringBuilderExample.java
@@ -5,25 +5,26 @@ public class StringBuilderExample {
 		StringBuilder sb = new StringBuilder();
 		
 		sb.append("Java " );
-		sb.append("Program Study");
+		sb.append("Program Study");  //문자열을 끝에 추가
 		System.out.println(sb.toString());
 		
-		sb.insert(4,  "2");
+		sb.insert(4,  "2");			//index4 위치 뒤에 2를 삽입
 		System.out.println(sb.toString());
 		
-		sb.setCharAt(4, '6');
+		sb.setCharAt(4, '6');			//index4 위치의 문자를 6으로 변경
 		System.out.println(sb.toString());
 		
-		sb.replace(6, 13, "Book");
+		sb.replace(6, 13, "Book");  		//index6부터 index13 '전'까지를 "Book"문자열로 대치
 		System.out.println(sb.toString());
 		
-		sb.delete(4, 5);
-		System.out.println(sb.toString());
+		sb.delete(4, 5);  //index 4 ~ index 5 전 까지 삭제
+		System.out.println(sb.toString()); 
 		
-		int length = sb.length();
+		int length = sb.length();  		//총 묹수 얻기
 		System.out.println("총문자수 : " + length);
 		
-		String result = sb.toString();
+		String result = sb.toString();  //버퍼에 있는 것을 String타입으로 리턴
+		
 		System.out.println(result);
 	}
 }
